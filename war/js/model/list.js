ff2b5e5654f4d225d5e0b3423a8f2300b27e8e8b
Jauhen <jauhen@gmail.com>
2012-11-17 12:43:40 -0800

    switch to collection

--- war/js/model/list.js
+++ war/js/model/list.js
@@ -1,27 +1,26 @@
-// var memes = [];
-// for (var i = 0; i < 10; i++) {
-//   memes.push(new Meme({
-//     url: 'https://memegen.googleplex.com/memeimage?k=11637038',
-//     messages: [
-//       {text: 'This Meme', layout: 'top-center'},
-//       {text: 'Is Deprecated', layout: 'bottom-center'}]
-//   }));
-
-//   memes.push(new Meme({
-//     url: 'https://memegen.googleplex.com/memeimage?k=11567160',
-//     messages: [
-//       {text: 'HolidayS are comming!', layout: 'bottom-center'}]
-//   }));
-
-//   memes.push(new Meme({
-//     url: 'https://memegen.googleplex.com/memeimage?k=11485229',
-//     messages: [
-//       {text: 'Monogomous', layout: 'bottom-center'}]
-//   }));
-// }
-
 var memes = new Memes();
-memes.fetch();
+for (var i = 0; i < 10; i++) {
+  memes.push(new Meme({
+    url: 'https://memegen.googleplex.com/memeimage?k=11637038',
+    messages: [
+      {text: 'This Meme', layout: 'top-center'},
+      {text: 'Is Deprecated', layout: 'bottom-center'}]
+  }));
+
+  memes.push(new Meme({
+    url: 'https://memegen.googleplex.com/memeimage?k=11567160',
+    messages: [
+      {text: 'HolidayS are comming!', layout: 'bottom-center'}]
+  }));
+
+  memes.push(new Meme({
+    url: 'https://memegen.googleplex.com/memeimage?k=11485229',
+    messages: [
+      {text: 'Monogomous', layout: 'bottom-center'}]
+  }));
+}
+
+// memes.fetch();
 
 for (var i = 0; i < 30; i++) {
   var memeView = new MemeView({model: memes.at(i)});
