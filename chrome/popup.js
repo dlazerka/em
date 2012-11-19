e83cda715300141e23e08dc4203763970a4a2c83
Andrey Mormysh <amormysh@google.com>
2012-11-19 12:26:54 -0800

    Ask to login (if not logged in yet)

--- chrome/popup.js
+++ chrome/popup.js
@@ -6,27 +6,28 @@ go();
 function go() {
     var memes = new Memes();
     var max = bg.lastSeen || 0;
-    memes.fetch({success: function(m) {
+    memes.fetch({success:function (m) {
         for (var i = 0; i < m.length; i++) {
             ts = parseInt(m.at(i).get('timestamp'));
             if (ts > max) {
                 max = ts;
             }
-            var memeView = new MemeView({model: m.at(i)});
+            var memeView = new MemeView({model:m.at(i)});
             $('#main_area').append(memeView.render().$el);
         }
         console.log("max=" + max);
         bg.lastSeen = max;
         localStorage.setItem("lastSeen", bg.lastSeen);
-    }, data: { top: 5}});
+    }, data:{ top:5}, error:function () {
+        $('#main_area').append('<div><a href="http://epammeme.appspot.com" target="_blank" style="color: #0000cd; font-size: 16px">Login to track memes</a></div>');
+    }});
 }
-
 
 
 function fetchMemes() {
     var jsonfile = new XMLHttpRequest();
     jsonfile.open("GET", "http://epammeme.appspot.com/memes", true);
-    jsonfile.onreadystatechange = function() {
+    jsonfile.onreadystatechange = function () {
         if (jsonfile.readyState == 4) {
             if (jsonfile.status == 200) {
                 showMemes(JSON.parse(jsonfile.responseText));
