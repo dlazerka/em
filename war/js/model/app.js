ad137f71802008f5d5df24dc3fc716b605b3950c
Dzmitry Lazerka <dlazerka@gmail.com>
2012-11-28 01:21:16 -0800

    Fixed bug which didn't redirect user after meme deletion. Minor cleanup.

--- war/js/model/app.js
+++ war/js/model/app.js
@@ -7,20 +7,20 @@ var AppRouter = Backbone.Router.extend({
 
   initialize: function() {
     var memes = new Memes(MEMES_JSON);
-    this.onSuccessFeatchAll_(memes);
+    this.onSuccessFetchAll_(memes);
   },
 
   memes: function() {
     var memes = new Memes();
-    memes.fetch({success: $.proxy(this.onSuccessFeatchAll_, this)});
+    memes.fetch({success: $.proxy(this.onSuccessFetchAll_, this)});
   },
 
   getMeme: function(id) {
     var meme = new Meme({id: id});
-    meme.fetch({success: $.proxy(this.onSuccessFeatch_, this)});
+    meme.fetch({success: $.proxy(this.onSuccessFetch_, this)});
   },
 
-  onSuccessFeatchAll_: function(memes) {
+  onSuccessFetchAll_: function(memes) {
     $('#main_area').empty();
     for (var i = 0; i < memes.length; i++) {
       var memeView = new MemeView({model: memes.at(i), className: 'meme memeSmall'});
@@ -28,20 +28,18 @@ var AppRouter = Backbone.Router.extend({
     }
   },
 
-  onSuccessFeatch_: function(meme) {
+  onSuccessFetch_: function(meme) {
     var memeView = new MemeView({model: meme, className: 'meme memeBig'});
     $('#main_area').html(memeView.render(50).$el);
     $('#main_area').append('<br/>');
-    $('<button class="delete">Delete</button>').on('click', function() {
+    $('<button class="delete">Delete</button>').on('click', $.proxy(function() {
       meme.destroy({success: this.onSuccessDestroy_})
-    }).appendTo($('#main_area'));
+    }, this)).appendTo($('#main_area'));
   },
 
   onSuccessDestroy_: function() {
-    setTimeout(function() {
-      Backbone.history.navigate('#memes', true);
-    }, 1000);
-  }  
+	Backbone.history.navigate('#memes', true);
+  }
 });
 
 $.ajaxSetup({cache: false});
