a578d82146a4ed72e8d36ef8293407bbb02dfaa4
Dzmitry Lazerka <dlazerka@gmail.com>
2012-12-29 11:54:27 -0800

    try

---
+++ test/hello_test.js
@@ -0,0 +1,6 @@
+CreateTest = TestCase("CreateTest");
+
+GreeterTest.prototype.testGreet = function() {
+  var greeter = new myapp.Greeter();
+  assertEquals("Hello World!", greeter.greet("World"));
+};
