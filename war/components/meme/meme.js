6406901a1a2572394273a34d65959714a8628fa0
Dzmitry Lazerka <dlazerka@gmail.com>
2012-12-28 20:57:44 -0800

    Memes extracted to new file.

--- war/components/meme/meme.js
+++ war/components/meme/meme.js
@@ -48,28 +48,6 @@ var Meme = Backbone.Model.extend({
   }
 });
 
-var Memes = Backbone.Collection.extend({
-  model: Meme,
-  url: '/memes',
-  page: 0,
-  filter: 'popular',
-
-  setFilter: function(newFilter) {
-    if (newFilter != this.filter) {
-      this.filter = newFilter;
-      this.page = 0;
-    }
-  },
-
-  getParams: function() {
-    return {
-      filter: this.filter,
-      page: this.page
-    }
-  }
-
-});
-
 var MemeView = Backbone.View.extend({
   tagName: 'div',
   className: 'meme memeSmall',
---
+++ war/components/meme/memes.js
@@ -0,0 +1,21 @@
+var Memes = Backbone.Collection.extend({
+  model: Meme,
+  url: '/memes',
+  page: 0,
+  filter: 'popular',
+
+  setFilter: function(newFilter) {
+    if (newFilter != this.filter) {
+      this.filter = newFilter;
+      this.page = 0;
+    }
+  },
+
+  getParams: function() {
+    return {
+      filter: this.filter,
+      page: this.page
+    }
+  }
+
+});
