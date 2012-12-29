89ca15af614ea74ce44f41e828fc4676c17a3b59
Dzmitry Lazerka <dlazerka@gmail.com>
2012-12-29 12:20:35 -0800

    Revert "try": supposed to be commited in a branch.
    This reverts commit a578d82146a4ed72e8d36ef8293407bbb02dfaa4.

--- test/hello_test.js
+++
@@ -1,6 +0,0 @@
-CreateTest = TestCase("CreateTest");
-
-GreeterTest.prototype.testGreet = function() {
-  var greeter = new myapp.Greeter();
-  assertEquals("Hello World!", greeter.greet("World"));
-};
