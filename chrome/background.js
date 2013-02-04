f38e19f68eeebad9a8f44b42775f0624c7ac4e6f
Dzmitry Lazerka <dlazerka@gmail.com>
2013-02-04 00:56:55 -0800

    Refactored plugin: now it depends on MemesStatsServlet, and doesn't show memes itself. On click it just opens a website.

--- chrome/background.js
+++ chrome/background.js
@@ -1,36 +1,32 @@
-// Called when a message is passed.  We assume that the content script
-// wants to show the page action.
-function onRequest(request, sender, sendResponse) {
-    // Show the page action for the tab that the sender (content script)
-    // was on.
-    chrome.pageAction.show(sender.tab.id);
-
-    // Return nothing to let the connection be cleaned up.
-    sendResponse({});
-}
-;
-
-// Listen for the content script to send a message to the background page.
-chrome.extension.onRequest.addListener(onRequest);
-
-var fetchFreq = 60000;
-var lastSeen = localStorage.getItem("lastSeen") || 0;
-console.log("lastSeen=" + lastSeen);
-
-getMemes();
-setInterval(getMemes, fetchFreq);
-
+const URL = 'http://epammeme.appspot.com/';
+//const URL = 'http://localhost:8888/';
+const URL_STATS = URL + 'memes/stats';
 function getMemes() {
-    var memes = new Memes();
-    memes.fetch({success:function (m) {
-        var badgeText = '';
-        if (m.length > 0) {
-            badgeText = '' + m.length;
-        }
-        chrome.browserAction.setBadgeText({text:badgeText});
-//        for (var i = 0; i < m.length; i++) {
-//            ts = parseInt(m.at(i).get('timestamp'));
-//            console.log(ts);
-//        }
-    }, data: { since: lastSeen}});
+  var lastSeen = Number(localStorage.getItem('lastSeen')) || 0;
+  $.ajax({
+    method: 'GET',
+    url: URL_STATS,
+    data: {'since': lastSeen},
+    processData: true,
+    dataType: 'json',
+  })
+  .success(function(data, textStatus, jqXhr) {
+    if (data.reset) {
+      localStorage.removeItem('lastSeen');
+      chrome.browserAction.setBadgeText({text: ''});
+      return;
+    }
+    var count = Number(data.count) || '';
+    chrome.browserAction.setBadgeText({text: '' + count});
+  });
 }
+
+setInterval(getMemes, 60000);
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  localStorage.setItem('lastSeen', new Date().getTime());
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.tabs.create({
+    url: URL,
+  });
+});
--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,1 +1,7 @@
-$('#extensionAd').append($('<div id="extensionInstalled">'));
+// Adds element to HTML page so that it knows this extension is installed.
+var ad = document.getElementById('extensionAd');
+if (ad) {
+  var div = document.createElement('div');
+  div.id = 'extensionInstalled';
+  ad.appendChild(div);
+}
